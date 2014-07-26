c41d39fa2d8d6d990594a70005155e6dd7128c56
sam.wang <smart.wolfer@gmail.com>
2014-07-26 15:46:33 +0800

    no message

---
+++ themes/sealscript/theme.py
@@ -0,0 +1,14 @@
+name = 'sealscript'
+author = 'Lyric'
+homepage = 'https://github.com/whtsky/catsup-theme-sealscript'
+post_per_page = 3
+vars = {
+    "github": "whtsky",
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "Awesome!"
+        }
+    ]
+}
